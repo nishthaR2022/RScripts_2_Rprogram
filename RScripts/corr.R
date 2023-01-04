47fda1697389dff161f8e8d967ba6e41f5e87ef0
Nishtha Gupta <119786582+nishthaR2022@users.noreply.github.com>
2023-01-05 01:10:42 +0530

    Add files via upload

---
+++ RScripts/corr.R
@@ -0,0 +1,14 @@
+corr <- function(directory, threshold=0) {
+
+  
+  library(data.table)
+  # Reading in all files and making a large data.table
+  lst <- lapply(file.path(directory, list.files(path = directory, pattern="*.csv")), data.table::fread)
+  dt <- rbindlist(lst)
+  
+  dt <- dt[complete.cases(dt),]
+  
+  # Apply threshold
+  dt <- dt[, .(nobs = .N, corr = cor(x = sulfate, y = nitrate)), by = ID][nobs > threshold]
+  return(dt[, corr])
+}
